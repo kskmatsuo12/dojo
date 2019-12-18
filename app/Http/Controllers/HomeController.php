c91cd54dc4368f70399c06faf0a01c2ae624ba17
Keisuke Matsuo <ksk.matsuo12@gmail.com>
2019-12-18 17:25:27 +0900

    ホームコントローラー合体！

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use Validator;
 use Illuminate\Http\Request;
+
+use App\User;
 
 class HomeController extends Controller
 // Userコントローラーとして使う。
@@ -32,18 +35,82 @@ class HomeController extends Controller
     {
         return view('users/home');
     }
+
+    // 三島さん作ユーザープロフィール
+
+    public function profile(Request $request)
+    {
+        //バリデーション
+        // $validator = Validator::make($request->all(), [
+        //     'user_name_sei' => 'required|max:20',
+        //     'user_name_mei' => 'required|max:20',
+        //     'user_hurigana_sei' => 'required|max:20',
+        //     'user_hurigana_mei' => 'required|max:20',
+        //     'email' => 'required|min:4',
+        //     'password' => 'required|min:6|max:20',
+        // ]);
+        //上あとでつかう
+        // バリデーション:エラー
+        // if ($validator->fails()) {
+        //     return redirect('welcome')
+        //         ->withInput()
+        //         ->withErrors($validator);
+        // }
+        //以下に登録処理を記述（Eloquentモデル）
+        $users = new User();
+        $users->user_name_sei = $request->user_name_sei;
+        $users->user_name_mei = $request->user_name_mei;
+        $users->user_hurigana_sei = $request->user_hurigana_sei;
+        $users->user_hurigana_mei = $request->user_hurigana_mei;
+        $users->email = $request->email;
+        $users->password = $request->password;
+        $users->save();
+        $last_insert_id = $users->id;
+        return view('users/profile', [
+            "lid"=>$last_insert_id
+        ]);
+        // return view(‘users/profile/.$user->id)’);
+    }
+    // ユーザープロフィール2
+    public function profile2(Request $request)
+    {
+        //バリデーション
+        // $validator = Validator::make($request->all(), [
+        //     ‘id’ => ‘required’,
+        // ]);
+        //バリデーション:エラー
+        // if ($validator->fails()) {
+        //     return redirect(‘welcome’)
+        //         ->withInput()
+        //         ->withErrors($validator);
+        // }
+        $users = User::find($request->lid);
+        $users->user_prefectures = $request->user_prefectures;
+        $users->user_exp_business = $request->user_exp_business;
+        $users->user_exp_job = $request->user_exp_job;
+        $users->user_phone = $request->user_phone;
+        $users->user_exp_company = $request->user_exp_company;
+        $users->user_exp_department = $request->user_exp_department;
+        $users->user_exp_position = $request->user_exp_position;
+        $users->user_exp_start = $request->user_exp_start;
+        $users->user_exp_end = $request->user_exp_end;
+        $users->user_current = $request->user_current;
+        $users->save();
+        return view('welcome');
+        // return view(‘users/profile2’);
+    }
     
     // ユーザープロフィール1
-    public function profile()
-    {
-        return view('users/profile');
-    }
+    // public function profile()
+    // {
+    //     return view('users/profile');
+    // }
 
     // ユーザープロフィール２
-    public function profile2()
-    {
-        return view('users/profile2');
-    }
+    // public function profile2()
+    // {
+    //     return view('users/profile2');
+    // }
 
     // ユーザープロフィール3
     public function profile3()
