cd93a5555cbacaf024e9f721efe87f069d4568fd
yosuke <yousuke.aoki005@gmail.com>
2019-12-18 14:15:30 +0900

    client_home画面作成　表示にはjobs_tableからデータ読み込みが必要

--- resources/views/clients/home.blade.php
+++ resources/views/clients/home.blade.php
@@ -1,3 +1,7 @@
+<!-- ページ読み込み時の注意事項 -->
+<!-- データが存在する状態で再度確認 -->
+<!-- foreachは要確認 -->
+
 @extends('layouts.app')
 <head>
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
@@ -12,6 +16,62 @@
 @section('content')
 <div class="container">
     <!-- この中にコンテンツ -->
+    <p>memo 項目　募集中案件の状況</p>
+    <div>
+        <div>案件一覧</div>
+        <div>公募案件の表示</div>
+        <div>foreachでもってくる案件一覧</div>
+        <div>表示項目　公募タイトル、相談したいこと、所用時間、担当者名　テーブルで作成</div>
+        <div>
+        <!-- 現在の案件 -->
+            @if (count($jobs) > 0)
+            <!-- クライアント条件での抜出要 -->
+            <div class="card-body">
+                <div class="card-body">
+                <table class="table table-striped task-table">
+                    <!-- テーブルヘッダ -->
+                    <thead>
+                    <th>公募タイトル</th>
+                    <th>相談したいこと</th>
+                    <th>所用時間</th>
+                    <th>担当者名</th>
+                    <th>案件進捗</th>
+                    <!-- <th>&nbsp;</th> -->
+                    </thead>
+                    <!-- テーブル本体 -->
+                    <tbody>
+                    @foreach ($jobs as $job)
+                    <!-- いけるか不安 -->
+                        <tr>
+                        <td class="table-text">
+                            <div>job_title</div>
+                            <!-- <div>{{ $job->job_title }}</div> -->
+                        </td>
+                        <td class="table-text">
+                            <div>{{ $job->consultation }}</div>
+                        </td>
+                        <td class="table-text">
+                            <div>{{ $job->work_term }}</div>
+                        </td>
+                        <td class="table-text">
+                            <div>{{ $job->responsible_party }}</div>
+                        </td>
+                        <td class="table-text">
+                            <div>＊＊案件進捗状態の表示</div>
+                        </td>
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+                </div>
+            </div>
+
+            @endif
+        </div>
+
+    </div>
+
+    <div>公募の作成（公募作成画面に遷移）</div>
 </div>
 
 <!-- JSファイルの指定してください！ -->
