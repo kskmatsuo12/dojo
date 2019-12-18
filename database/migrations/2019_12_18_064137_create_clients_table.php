7ae4a7776e9fa104570462c8b75211b9550504b0
Keisuke Matsuo <ksk.matsuo12@gmail.com>
2019-12-18 16:14:04 +0900

    マイグレーションファイルの暫定の完成！

---
+++ database/migrations/2019_12_18_064137_create_clients_table.php
@@ -0,0 +1,44 @@
+
+    <?php
+        use Illuminate\Support\Facades\Schema;
+        use Illuminate\Database\Schema\Blueprint;
+        use Illuminate\Database\Migrations\Migration;
+        
+        class CreateClientsTable extends Migration
+        {
+            /**
+             * Run the migrations.
+             *
+             * @return void
+             */
+            public function up()
+            {
+                Schema::create("clients", function (Blueprint $table) {
+
+						$table->increments('id');
+						$table->string('client_name',30)->nullable();
+						$table->string('client_kana',30)->nullable();
+						$table->string('client_id',20)->nullable();
+						$table->string('client_pass',20)->nullable();
+						$table->string('client_loc',20)->nullable();
+						$table->text('client_url')->nullable();
+						$table->string('client_biz',5000)->nullable();
+						$table->integer('client_num_emp')->nullable();
+						$table->string('client_matter',5000)->nullable();
+						$table->timestamps();
+						$table->softDeletes();
+
+                });
+            }
+
+            /**
+             * Reverse the migrations.
+             *
+             * @return void
+             */
+            public function down()
+            {
+                Schema::dropIfExists("clients");
+            }
+        }
+    
---
+++ database/migrations/2019_12_18_064137_create_messages_groups_table.php
@@ -0,0 +1,39 @@
+
+    <?php
+        use Illuminate\Support\Facades\Schema;
+        use Illuminate\Database\Schema\Blueprint;
+        use Illuminate\Database\Migrations\Migration;
+        
+        class CreateMessagesGroupsTable extends Migration
+        {
+            /**
+             * Run the migrations.
+             *
+             * @return void
+             */
+            public function up()
+            {
+                Schema::create("messages_groups", function (Blueprint $table) {
+
+						$table->increments('id');
+						$table->integer('group_id')->nullable();
+						$table->string('group_message',1000)->nullable();
+						$table->integer('group_messages_from')->nullable();
+						$table->integer('group_message_which')->nullable();
+						$table->timestamps();
+						$table->softDeletes();
+
+                });
+            }
+
+            /**
+             * Reverse the migrations.
+             *
+             * @return void
+             */
+            public function down()
+            {
+                Schema::dropIfExists("messages_groups");
+            }
+        }
+    
