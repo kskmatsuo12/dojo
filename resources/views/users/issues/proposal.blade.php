b88a34dd202d1a60128ba4247055e38176fbaf87
yosuke <yousuke.aoki005@gmail.com>
2019-12-19 12:16:08 +0900

    user/index,user/proposal修正中

--- resources/views/users/issues/proposal.blade.php
+++ resources/views/users/issues/proposal.blade.php
@@ -12,6 +12,17 @@
 @section('content')
 <div class="container">
     <!-- この中にコンテンツ -->
+    <div>
+        <h1>企業様へのメッセージ</h1>
+        <p>
+            当案件において、現時点でどんなことで力になれそうか、また今回の取り組みどんなことが学びたいかご記入ください。
+        </p>
+    </div>
+    <div>
+        <form action="{{ url('profile2Store') }}" method="POST">
+        <input type="textarea" name="suggestion_text" value="{{old('suggestion_text')}}" class="form-control" placeholder="要検討">
+        </form>   
+    </div>
 </div>
 
 <!-- JSファイルの指定してください！ -->
