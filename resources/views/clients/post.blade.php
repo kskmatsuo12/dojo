a5fcbcace990fd358bc83e3f418a89d4b27ad879
mishi <missy340168@gmail.com>
2019-12-19 16:47:02 +0900

    各フォームのスタイルつけました

--- resources/views/clients/post.blade.php
+++ resources/views/clients/post.blade.php
@@ -17,63 +17,55 @@
     <!-- この中にコンテンツ -->
     <div>
         <!-- <label for="sei" class="">案件のタイトル</label> -->
-        <input type="text" name="job_title" value="{{old('sei')}}" placeholder="案件のタイトル">
+        <input type="text" name="job_title" value="{{old('job_title')}}" placeholder="プロジェクトタイトル">
     </div>
     <div>
     <!-- <label for="mei" class="">案件の概要</label> -->
-        <input type="text" name="job_text" value="{{old('mei')}}" placeholder="案件の概要">
+        <input type="text" name="job_text" value="{{old('job_text')}}" placeholder="プロジェクト概要">
     </div>
     <div>
-        <!-- <label for="mei_kana" class="">相談したいこと</label> -->
-        <input type="text" name="recruit_advisor" value="{{old('mei_kana')}}" placeholder="相談したいこと">
+    <!-- <label for="mei" class="">案件の概要</label> -->
+        <input type="text" name="recruit_advisor" value="{{old('recruit_advisor')}}" placeholder="探しているアドバイザー">
     </div>
     <div>
-        <!-- <label for="mei_kana" class="">相談したいこと</label> -->
-        <input type="text" name="consultation" value="{{old('mei_kana')}}" placeholder="相談したいこと">
-    </div>
-    <div">
         <!-- <label for="email" class="">提案時に記載いただきたいこと</label> -->
-        <input type="text" name="request_fill_out" value="{{old('email')}}" placeholder="提案時に記載いただきたいこと">
+        <input type="text" name="request_fill_out" value="{{old('request_fill_out')}}" placeholder="提案時に記載いただきたいこと">
     </div>
     <div>
         <!-- <label for="pw" class="">相談の形式</label> -->
-        <input type="text" name="work_format" value="{{old('pw')}}" placeholder="相談の形式">
+        <input type="text" name="work_format" value="{{old('work_format')}}" placeholder="相談の形式">
     </div>
     <div>
         <!-- <label for="pw" class="">相談の期間・時間</label> -->
-        <input type="text" name="work_term" value="{{old('pw')}}" placeholder="相談の期間・時間">
+        <input type="text" name="work_term" value="{{old('work_term')}}" placeholder="相談の期間・時間">
     </div>
     <div>
         <!-- <label for="pw" class="">面談方法</label> -->
-        <input type="text" name="interview_format" value="{{old('pw')}}" placeholder="面談方法">
+        <input type="text" name="interview_format" value="{{old('interview_format')}}" placeholder="面談方法">
     </div>
     <div>
         <!-- <label for="pw" class="">面談場所</label> -->
-        <input type="text" name="interview_place" value="{{old('pw')}}" placeholder="面談場所">
+        <input type="text" name="interview_place" value="{{old('interview_place')}}" placeholder="面談場所">
     </div>
     <div>
         <!-- <label for="pw" class="">募集人数</label> -->
-        <input type="text" name="request_number" value="{{old('pw')}}" placeholder="募集人数">
+        <input type="text" name="request_number" value="{{old('request_number')}}" placeholder="募集人数">
     </div>
     <div>
         <!-- <label for="pw" class="">募集期限</label> -->
-        <input type="date" name="recruitment_term" value="{{old('pw')}}" placeholder="募集期限">
+        <input type="date" name="recruitment_term" value="{{old('recruitment_term')}}" placeholder="募集期限">
     </div>
     <div>
         <!-- <label for="pw" class="">担当者名</label> -->
-        <input type="text" name="responsible_party" value="{{old('pw')}}" placeholder="担当者名">
+        <input type="text" name="responsible_party" value="{{old('responsible_party')}}" placeholder="担当者名">
     </div>
     <div>
         <!-- <label for="pw" class="">担当者のメールアドレス</label> -->
-        <input type="text" name="responsible_email" value="{{old('pw')}}" placeholder="担当者のメールアドレス">
-    </div>
-    <div>
-        <!-- <label for="pw" class="">得られるスキル</label> -->
-        <input type="text" name="get_skill" value="{{old('pw')}}" placeholder="得られるスキル">
+        <input type="text" name="responsible_email" value="{{old('responsible_email')}}" placeholder="担当者のメールアドレス">
     </div>
     <div>
         <!-- <label for="pw" class="">client_id</label> -->
-        <input type="text"  name="client_id" value="{{old('pw')}}" placeholder="client_id">
+        <input type="hidden"  name="client_id" value="{{old('client_id')}}" placeholder="client_id">
     </div>
 
     <div class="form-group">
--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -3,199 +3,188 @@
 <!-- foreachは要確認 -->
 <!-- テーブルがない場合、↓を読み込み -->
 <?php
-
 $suggestions=[];
-
-
 ?>
-@include('layouts/header')
-
 
 @extends('layouts.app')
+
 <head>
     <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
     <link rel="stylesheet" href="{{ asset('css/common.css') }}">
-
-    <!-- CSSファイル指定してください -->
-
     <link rel="stylesheet" href="{{ asset('css/users/home.css') }}">
-
-    <!-- CSSファイル指定してください -->
 </head>
+<style>
+body{
+  background:#f2feff;
+}
 
+.contains{
+    display: flex;
+    justify-content: center;
+    margin:20px auto;
+    width:90%
+}
+
+.pjts{
+    float: left;
+    position: relative;
+    width:145px;
+    text-align: center;
+    padding:10px;
+    margin-bottom:120px;
+}
+
+.pjt img{
+    width:144px;
+}
+
+.info{
+    position: absolute;
+    top: 140%;
+    left: 50%;
+    width:125px;
+    height:100px;
+    padding: 10px;
+    background: white;
+    -ms-transform: translate(-50%,-50%);
+    -webkit-transform: translate(-50%,-50%);
+    transform: translate(-50%,-50%);
+}
+
+.pjt_term{
+    font-size: 13px;
+    font-weight: bold;
+    color: rgb(67,196,207);
+    margin:5px 0 10px;
+
+}
+.pjt_title,.pjt_name{
+    font-size: 13px;
+    font-weight: bold;
+    color: rgb(37, 37, 37);
+    margin:5px 0 5px;
+}
+.line h1{
+    padding-top:20px;
+    text-align: center;
+    height: 50px;
+    line-height: 50px;
+    font-size: 18px;
+    font-weight: bold;
+    color:rgb(67,196,207);
+    letter-spacing:1px;
+}
+
+.line h2{
+    text-align: center;
+    background: rgb(67,196,207);
+    height: 5px;
+    width:50px;
+    margin: 0 auto 20px;
+    border-radius: 50px;
+}
+
+@media screen and (min-width: 781px) {
+    .contains{
+        display: flex;
+        justify-content: center;
+        margin:20px auto;
+        width:80%
+    }
+    .pjts{
+        float: left;
+        position: relative;
+        width:210px;
+        text-align: center;
+        padding:10px;
+        margin-bottom:120px;
+    }
+
+    .pjt img{
+        width:210px;
+    }
+
+    .info{
+        position: absolute;
+        top: 125%;
+        left: 50%;
+        width:190px;
+        height:100px;
+        padding: 10px;
+        background: white;
+        -ms-transform: translate(-50%,-50%);
+        -webkit-transform: translate(-50%,-50%);
+        transform: translate(-50%,-50%);
+    }
+}
+@media screen and (min-width: 1040px) {
+  .container {
+    width: 1040px;
+    margin: auto;
+    width:90%;
+  }
+
+}
+
+</style>
 <!-- CSSファイルの指定をしてください〜 -->
 @section('content')
-
 <div class="container">
-    <!-- この中にコンテンツ -->
-    <p>memo 項目　募集中案件の状況</p>
 
-    <!-- <h1>aaa</h1> -->
-    <div>
-        <div><h1>現在進捗中の案件</h1></div>
+    <div class="line">
+        <h1>現在進捗中</h1>
+        <h2>　　</h2>
+    </div>
 
-        <div>
-        <!-- 現在の案件 foreach -->
-            @if (count($suggestions) > 0)
-            <!-- クライアント条件での抜出要 -->
-            <div class="">
-                <div class="">
-                @foreach ($suggestions as $suggestion)
-                <table class="">
-                    <!-- テーブル本体 -->
-                    <tbody>
-                    <tr class="">   
-                    <td class="">
-                        <div>公募タイトル</div>
-                    </td>
-                    <td class="">
-                        <div>{{$suggestion->job_title}}</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div>相談したいこと</div>
-                    </td>
-                    <td class="">
-                        <div>{{$suggestion->consultation}}</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div>募集期限</div>
-                    </td>
-                    <td class="">
-                        <div>{{$suggestion->recruitment_term}}</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div>進捗状況</div>
-                    </td>
-            
-                    </tr>
-                </tbody>
-            </table>
-            @endforeach
+    <div class="contains">
+        @if (count($suggestions) > 0)
+                <div class="contain">
+                    @foreach ($suggestions as $suggestion)
+                    <div class="pjts">
+                    <a href="issues/<?php echo $job->id ?>">
+                        <div class="pjt">
+                            <img src="https://static.camp-fire.jp/uploads/project_version/image/331374/5fd91b4a-a70b-40fe-ae2c-1545fa0250fa.jpg?ixlib=rails-2.1.4&w=1024&h=682&fit=clip&auto=format" alt="">
+                            <div class="info">
+                                <p class="pjt_term">{{$suggestion->recruitment_term}}</p>
+                                <p class="pjt_title">{{$suggestion->job_title}}</p>
+                                <p class="pjt_name">{{$suggestion->consultation}}</p>
+                            </div>
+                        </div>
+                    </a>
+                    </div>
+                    @endforeach
                 </div>
             </div>
-            @endif
         </div>
-        <div>
-        <!-- 現在の案件　表記例 -->
-            <table class="">
-                <!-- テーブル本体 -->
-                <tbody>
-                    <tr class="">   
-                    <td class="">
-                        <div>公募タイトル</div>
-                    </td>
-                    <td class="">
-                        <div>$suggestion->job_title</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div>相談したいこと</div>
-                    </td>
-                    <td class="">
-                        <div>$suggestion->consultation</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div>募集期限</div>
-                    </td>
-                    <td class="">
-                        <div>$suggestion->recruitment_term</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div>進捗状況</div>
-                    </td>
-                    
-                    </tr>
-                </tbody>
-            </table>
-        </div>
+        @endif
     </div>
 
-    <div>
-        <!-- 新着案件　表記例 -->
-        <div><h1>新着案件</h1></div>
-        <div>
-        @if (count($jobs) > 0)
-        @foreach ($jobs as $job)
-        <table class="">
-                <!-- テーブル本体 -->
-                <tbody>
-                    <tr class="">   
-                    <td class="">
-                        <div>公募タイトル</div>
-                    </td>
-                    <td class="">
-                        <div>{{$job->job_title}}</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div>募集期限</div>
-                    </td>
-                    <td class="">
-                        <div>{{$job->recruitment_term}}</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div><a href="">確認:案件idもって案件詳細へ</a></div>
-                    </td>
-                    <td></td>
-                    </tr>
-                </tbody>
-            </table>
+
+    <div class="line">
+        <h1>新着</h1>
+        <h2>　　</h2>
+    </div>
+
+    <div class="contains">
+    @if (count($jobs) > 0)
+        <div class="contain">
+            @foreach ($jobs as $job)
+            <div class="pjts">
+                <div class="pjt">
+                    <img src="https://static.camp-fire.jp/uploads/project_version/image/331374/5fd91b4a-a70b-40fe-ae2c-1545fa0250fa.jpg?ixlib=rails-2.1.4&w=1024&h=682&fit=clip&auto=format" alt="">
+                    <div class="info">
+                        <p class="pjt_term">{{$job->recruitment_term}}</p>
+                        <p class="pjt_title">{{$job->job_title}}</p>
+                        <p class="pjt_name">{{$job->consultation}}</p>
+                    </div>
+                </div>
+            </div>
             @endforeach
         </div>
         @endif
     </div>
 
-    <div>
-        <div><h1>あなたへのおすすめ</h1></div>
-        <div>
-        @if (count($jobs) > 0)
-        @foreach ($jobs as $job)
 
-        <table class="">
-                <!-- テーブル本体 -->
-                <tbody>
-                    <tr class="">   
-                    <td class="">
-                        <div>公募タイトル</div>
-                    </td>
-                    <td class="">
-                        <div>{{$job->job_title}}</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div>募集期限</div>
-                    </td>
-                    <td class="">
-                        <div>{{$job->recruitment_term}}</div>
-                    </td>
-                    </tr>
-                    <tr class="">
-                    <td class="">
-                        <div><a href="">確認:案件idもって案件詳細へ</a></div>
-                    </td>
-                    <td></td>
-                    </tr>
-                </tbody>
-            </table>
-            @endforeach
-        </div>
-        @endif
-    </div>
 </div>
 @include('layouts/sp_menu')
 
