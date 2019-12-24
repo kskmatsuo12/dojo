19138a6b2acc9fc6b141c6392ad6705619b9855c
yosuke <yousuke.aoki005@gmail.com>
2019-12-24 12:52:21 +0900

    ユーザープロフィール、ウェルカム修正

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -264,7 +264,7 @@ body{
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::user()->name }} {{Auth::user()->user_name_mei}} 様<span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
